c0d1b366def2f710acf237455e0c446b95f5bcbc
ravindukariyapperuma <ravindu.kariyapperuma@gmail.com>
2020-10-27 22:33:27 +0530

    interpolation

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -3,19 +3,28 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: '[app-test]',
   template: `<div>
-                Inline template
-              </div>`,
-  styles: [`
-    div {
-      color: red;
-    }
-  `]
+                Welcome {{name}}
+              </div>
+              <h2>{{2+2}}</h2>
+              <h2>{{"Welcome " + name}}</h2>
+              <h2>{{name.length}}</h2>
+              <h2>{{name.toUpperCase()}}</h2>
+              <h2>{{greetUser()}}</h2>
+              <h2>{{siteUrl}}</h2>
+  `,
+  styleUrls: ['./test.component.css']
 })
 export class TestComponent implements OnInit {
 
+  public name = "Lakshan";
+  public siteUrl = window.location.href;
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  greetUser(){
+    return "Hello " + this.name;
   }
 
 }
