8733b9aa973063745ba6fb447bc9fefb8af7129f
ravindukariyapperuma <ravindu.kariyapperuma@gmail.com>
2020-10-27 21:00:20 +0530

    inline template multiline

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -2,7 +2,9 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: '[app-test]',
-  template: '<div>Inline template</div>',
+  template: `'<div>
+                Inline template
+              </div>'`,
   styleUrls: ['./test.component.css']
 })
 export class TestComponent implements OnInit {
